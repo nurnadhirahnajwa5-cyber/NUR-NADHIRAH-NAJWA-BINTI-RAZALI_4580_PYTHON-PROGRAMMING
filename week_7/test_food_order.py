4ed9c1f997d13f751cf3a33f0d9380ff742c8241
nurnadhirahnajwa5-cyber <nurnadhirahnajwa5@gmail.com>
2026-07-08 07:03:49 +0000

    week_7

---
+++ week_7/test_food_order.py
@@ -0,0 +1,19 @@
+from food_order import calculate_total
+
+def test_order1():
+    assert calculate_total(10, 2) == 20
+
+def test_order2():
+    assert calculate_total(10, 3) == 30
+
+def test_order3():
+    assert calculate_total(20, 5) == 100
+
+def test_order4 ():
+    assert calculate_total(5, 2) == 10
+
+def test_invalid_price():
+    assert calculate_total(-10, 2) == "invalid price"
+
+def test_invalid_quantity():
+    assert calculate_total(10, -2) == "invalid quantity"
